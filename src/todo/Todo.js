1497d901331bbdda4cc73ee1917ded1e0f0c0712
akshatk806 <akshatk806@gmail.com>
2023-05-25 17:16:46 +0530

    2. Adding the tasks into the list

--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 
 const Todo = () => {
+    const [inputData, setInputData] = useState(""); 
+    const [items, setItems] = useState([]);
+
+    // function for adding the items in todo list
+    const addItem = ()=>{
+        if(!inputData) {
+            alert("Please fill the Task")
+        }
+        else {
+            setItems([...items, inputData])
+        }
+    }
+
   return (
     <>
         <div className="main-div">
             <div className="child-div">
+                <h1 style={{color:"white", marginBottom:"4rem", fontSize:"5rem"}}>Welcome to <span style={{color:"#f2bb0c"}}>TodoRocket</span> 🚀</h1>
                 <figure>
-                    <h1 style={{color:"white", marginBottom:"4rem", fontSize:"5rem"}}>Welcome to <span style={{color:"#f2bb0c"}}>TodoRocket</span> 🚀</h1>
                     <img src="./images/todo.svg" alt="todoimage" />
                     <figcaption>Add Your Task Here ✌️</figcaption>
                 </figure>
                 <div className="addItems">
-                    <input type="text" placeholder="✍️Add Task" className="form-control" />
-                    <i className="fa fa-plus add-btn"></i>
+                    <input type="text" placeholder="✍️Add Task" className="form-control" value={inputData} onChange={(e)=>setInputData(e.target.value)}/>
+                    <i className="fa fa-plus add-btn" onClick={addItem}></i>
                 </div>
+
+                {/* show our items */}
+                <div className="showItems">
+                    <div className="eachItem">
+                        <h3>Apple</h3>
+                        <div className="todo-btn">
+                        <i className="far fa-edit add-btn"></i>
+                        <i className="far fa-trash-alt add-btn"></i>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Remove All button */}
+
+
                 <div className="showItems">
                     <button className="btn effect04" data-sm-link-text="Remove All"><span>CHECK LIST</span></button>
                 </div>
